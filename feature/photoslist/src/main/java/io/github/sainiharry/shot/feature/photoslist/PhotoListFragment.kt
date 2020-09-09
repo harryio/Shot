6ad3de49f1466ec966a7378b54a1f3999a45e3b6
Harsimran Saini <harry.singh@mailbox.org>
2020-09-09 03:59:27 -0400

    Load data on photos list fragment

--- feature/photoslist/src/main/java/io/github/sainiharry/shot/feature/photoslist/PhotoListFragment.kt
+++ feature/photoslist/src/main/java/io/github/sainiharry/shot/feature/photoslist/PhotoListFragment.kt
@@ -1,10 +1,15 @@
 package io.github.sainiharry.shot.feature.photoslist
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import io.github.sainiharry.shot.repository.photos.PhotoRepository
+import kotlinx.android.synthetic.main.fragment_photo_list.*
+import kotlinx.coroutines.Dispatchers
 
 class PhotoListFragment : Fragment() {
 
@@ -15,8 +20,32 @@ class PhotoListFragment : Fragment() {
         object : ViewModelProvider.Factory {
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
                 @Suppress("UNCHECKED_CAST")
-                return PhotoListViewModel(photoRepository) as T
+                return PhotoListViewModel(photoRepository, Dispatchers.Main.immediate) as T
             }
+        }
+    }
+
+    private lateinit var adapter: PhotoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        model.loadData()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (!this::adapter.isInitialized) {
+            adapter = PhotoAdapter(model)
+        }
+
+        recycler_view.setHasFixedSize(true)
+        recycler_view.layoutManager = GridLayoutManager(requireContext(), 2)
+        recycler_view.adapter = this@PhotoListFragment.adapter
+
+        model.photoList.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
         }
     }
 }
--- feature/photoslist/src/main/java/io/github/sainiharry/shot/feature/photoslist/PhotoListViewModel.kt
+++ feature/photoslist/src/main/java/io/github/sainiharry/shot/feature/photoslist/PhotoListViewModel.kt
@@ -1,8 +1,33 @@
 package io.github.sainiharry.shot.feature.photoslist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.sainiharry.shot.common.ImageSource
+import io.github.sainiharry.shot.common.ItemClickListener
+import io.github.sainiharry.shot.common.Photo
 import io.github.sainiharry.shot.repository.photos.PhotoRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
 
-internal class PhotoListViewModel(private val photoRepository: PhotoRepository) : ViewModel() {
+internal class PhotoListViewModel(
+    private val photoRepository: PhotoRepository,
+    private val dispatcher: CoroutineDispatcher
+) : ViewModel(),
+    ItemClickListener<Photo> {
 
+    private val _photoList = MutableLiveData<List<Photo>>()
+    val photoList: LiveData<List<Photo>>
+        get() = _photoList
+
+    fun loadData() {
+        viewModelScope.launch(dispatcher) {
+            _photoList.value = photoRepository.fetchPhotos(ImageSource.UNSPLASH)
+        }
+    }
+
+    override fun onItemClick(obj: Photo) {
+        TODO("Not yet implemented")
+    }
 }
